eeca286064764c9eabe7e3862e16c1c53764ab97
Miguel Angel Ramos <return.infox@gmail.com>
2024-06-13 17:39:16 -0400

    en el controlador

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { Observable } from 'rxjs';
 import { Product } from './entities/product.entity';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -12,6 +13,23 @@ export class ProductsController {
   @Post()
   create(@Body() createProductDto:CreateProductDto): Observable<Product> {
     return this.productsService.create(createProductDto);
+  }
+
+  findAll(): Observable<Product[]> {
+    return this.productsService.findAll();
+  }
+
+  findOne(@Param('id') id:string):Observable<Product>{
+    //* + hace la transformacion a numeric
+    return this.productsService.findOne(+id);
+  }
+
+  update(@Param('id') id:string, @Body() updateProductDto: UpdateProductDto):Observable<any> {
+    return this.productsService.update(+id, updateProductDto);
+  }
+
+  remove(id: string): Observable<any> {
+   return this.productsService.remove(+id);
   }
 
 }
