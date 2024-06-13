cbc40b86fc84928ca5d62e2eb74d2b1a06b64217
Miguel Angel Ramos <return.infox@gmail.com>
2024-06-13 17:49:24 -0400

    configuracion de swagger en el main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
@@ -12,7 +14,20 @@ async function bootstrap() {
       // Rechaza las solicitudes que contengan propiedades que no estan definidas en los Dtos
       forbidNonWhitelisted: true
     })
-  )
+  );
+  // Configuración de Swagger
+  const config = new DocumentBuilder()
+  .setTitle('Product API')
+  .setDescription('API para la gestion productos (CRUD)')
+  .setVersion('1.0')
+  .addTag('productos')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Configurar el endpoint en el que servirá la documentación Swagger
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(envs.port);
   console.log(`Server running port: ${envs.port}`);
 }
